83d091fd6f9c7c7ae61a4a25b1dc675fd9aabe80
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-02 15:39:54 -0700

    display destination info other than forecast

--- spec/features/user_views_a_destination_spec.rb
+++ spec/features/user_views_a_destination_spec.rb
@@ -5,14 +5,27 @@ feature "user clicks a specific destination" do
 
     destination = Destination.create(
                               name: "Denver", zip: "80246", description: "great city",
-                              image_url: "www.denver.com/picture.jpg")
+                              image_url: "https://placehold.it/300x300.png/000")
     visit "/"
 
     expect(page).to have_content(destination.name)
+
+    find(:xpath, "//a/img[@alt='000']/..").click
+
+    expect(current_path).to eq(destination_path(destination))
+    expect(page).to have_content(destination.name)
+    expect(page).to have_content(destination.zip)
+    expect(page).to have_content(destination.description)
+    # expect(page).to have_content(destination.name)
+
+    # expect(page).to have_image(destination.image_url)
+    # click_link destination.image_url
+    #
   end
 end
 
-
+# /html/body/div/img
+# <img src="https://placehold.it/300x300.png/000" alt="000">
 # As a user
 # When I visit "/"
 # And I click on a destination
