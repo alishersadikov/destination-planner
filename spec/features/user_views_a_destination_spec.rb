23e6ea5f780f91796598d31d0824be25f49d7510
Alisher Sadikov <alishersadikov@gmail.com>
2017-01-02 16:56:34 -0700

    add expectations around data in the featre test

--- spec/features/user_views_a_destination_spec.rb
+++ spec/features/user_views_a_destination_spec.rb
@@ -14,14 +14,19 @@ feature "user clicks a specific destination" do
       find(:xpath, "//a/img[@alt='000']/..").click
 
       expect(current_path).to eq(destination_path(destination))
+
       expect(page).to have_content(destination.name)
       expect(page).to have_content(destination.zip)
       expect(page).to have_content(destination.description)
-      # expect(page).to have_content(destination.name)
 
-      # expect(page).to have_image(destination.image_url)
-      # click_link destination.image_url
-      #
+      expect(page).to have_content(Time.now.strftime("%B %-d, %Y"))
+      expect(page).to have_content("52")
+      expect(page).to have_content("18")
+      expect(page).to have_content("Partly Cloudy")
+      expect(page).to have_content("January 11, 2017")
+      expect(page).to have_content("46")
+      expect(page).to have_content("28")
+      expect(page).to have_content("Clear")
     end
   end
 end
